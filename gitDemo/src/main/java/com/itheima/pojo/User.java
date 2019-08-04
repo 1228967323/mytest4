5d7261539fa2a4dc38c1d79daa4c9fbb370c9bb0
adminstrator <1228967323@qq.com>
2019-08-04 09:14:12 +0800

    修改User

--- gitDemo/src/main/java/com/itheima/pojo/User.java
+++ gitDemo/src/main/java/com/itheima/pojo/User.java
@@ -7,4 +7,48 @@ package com.itheima.pojo;
 public class User {
     private String username ; //用户名
     private String password ; //密码
+    private Integer age; //年龄
+    private String number; //电话号码
+
+    public User() {
+    }
+
+    public User(String username, String password, Integer age, String number) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.number = number;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
